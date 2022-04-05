2b744e65f81009491033f285abd6d7e1608d1736
Jordy Arntz <hello@jordyarntz.com>
2022-04-05 13:44:06 +0200

    Add Placeholder

---
+++ src/components/Placeholder/Placeholder.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Placeholder.scss';
+
+const Placeholder = () => (
+  <div className="placeholder">
+    <h1>MRR Drones</h1>
+  </div>
+);
+
+export default Placeholder;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './shared.scss';
 
-const Home = () => <div className="page home" />;
+import Placeholder from '../components/Placeholder/Placeholder';
+
+const Home = () => (
+  <div className="page home">
+    <Placeholder />
+  </div>
+);
 
 export default Home;
