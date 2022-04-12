bea7f394e1bbd9be4456edc6774bf2aa96d888f2
liekevdvoort <lieke.voort1@gmail.com>
2022-04-12 15:10:05 +0200

    Props fix

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './Button.scss';
 import { motion } from 'framer-motion';
 
-export default function Button({ children, onclick }) {
+export default function Button({ children, onclick, fullwidth, red }) {
   return (
     <motion.button
       whileHover={{ scale: 1.05 }}
       transition={{ duration: 0.25 }}
       whileTap={{ scale: 0.75 }}
       onClick={onclick}
-      className={`button ${props.fullwidth ? 'fullwidth' : ''} ${props.red ? 'red' : ''}`}
+      className={`button ${fullwidth ? 'fullwidth' : ''} ${red ? 'red' : ''}`}
     >
       {children}
     </motion.button>
