4ce5e6fe85c15a3c0c112ecfba532b52ae9d15e3
Jordy Arntz <hello@jordyarntz.com>
2022-04-19 13:02:29 +0200

    Install FA6 and Turf.js

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Map.scss';
+
+// import * as turf from '@turf/turf';
+
+export default function Map() {
+  return <div className="Map" />;
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import './Home.scss';
 import Button from '../../components/Button/Button';
 import Input from '../../components/Input/Input';
 import PageWrapper from '../../components/PageWrapper/PageWrapper';
+import Map from '../../components/Map/Map';
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <Button>test</Button>
       <Input label="email" placeholder="test" />
       <Input white placeholder="test" />
+      <Map />
     </PageWrapper>
   );
 }
