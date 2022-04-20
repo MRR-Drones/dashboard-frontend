ded65ad1c98b6c01fffc212e7dc42072b54de9cc
liekevdvoort <lieke.voort1@gmail.com>
2022-04-20 15:26:58 +0200

    v1 card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Card.scss';
+
+// Import FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBatteryHalf, faTimer, faArrowsUpDown, faClockRotateLeft } from '@fortawesome/pro-solid-svg-icons';
+
+// Import components
+import StatusIndicator from './StatusIndicator/StatusIndicator';
+
+export default function Card({ number, status, small, battery, speed, altitude, timeTravelled }) {
+  return (
+    <div className="Card">
+      <div className="top">
+        <div className={`border-left ${status}`} />
+        <p className="name">Drone</p>
+        <p className="icon">{number}</p>
+        <StatusIndicator status={status} />
+        <p className="route">Inspection round 1</p>
+      </div>
+      {!small && (
+        <div className="bottom">
+          <div className="icon-text">
+            <FontAwesomeIcon icon={faBatteryHalf} />
+            <p>{battery} %</p>
+          </div>
+          <div className="icon-text">
+            <FontAwesomeIcon icon={faArrowsUpDown} />
+            <p>{altitude} m</p>
+          </div>
+          <div className="icon-text">
+            <FontAwesomeIcon icon={faClockRotateLeft} />
+            <p>{timeTravelled} min</p>
+          </div>
+          <div className="icon-text">
+            <FontAwesomeIcon icon={faTimer} />
+            <p>{speed} km/h</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import Button from '../../components/Button/Button';
 import Input from '../../components/Input/Input';
 import PageWrapper from '../../components/PageWrapper/PageWrapper';
 import Map from '../../components/Map/Map';
+import Card from '../../components/Card/Card';
 
 export default function Home() {
   return (
@@ -17,6 +18,8 @@ export default function Home() {
       <Input label="email" placeholder="test" />
       <Input white placeholder="test" />
       <Map />
+      <Card battery={20} speed={50} altitude={200} timeTravelled={23} number={1} status="active" />
+      <Card battery={20} speed={50} altitude={200} timeTravelled={23} number={2} status="offline" small />
     </PageWrapper>
   );
 }
