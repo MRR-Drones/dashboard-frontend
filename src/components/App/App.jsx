f9949dcf475d9dadf1e53113c95e175e1231b6cd
Jordy Arntz <hello@jordyarntz.com>
2022-05-10 13:20:26 +0200

    Convert to React Router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import './App.scss';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from '../../pages/Home/Home';
 import Overview from '../../pages/Overview/Overview';
-
-import Route from '../../core/Route';
+import SideNav from './SideNav/SideNav';
 
 export default function App() {
   return (
     <div className="app">
-      <Route path="/">
-        <Home />
-      </Route>
-      <Route path="/overview">
-        <Overview />
-      </Route>
+      <Router>
+        <SideNav />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/overview" element={<Overview />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
