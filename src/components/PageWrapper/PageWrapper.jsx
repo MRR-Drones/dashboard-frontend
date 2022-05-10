814557005a6ce27e301d8ec38fc215df31463474
liekevdvoort <lieke.voort1@gmail.com>
2022-05-10 13:52:26 +0200

    fix padding overview

--- src/components/PageWrapper/PageWrapper.jsx
+++ src/components/PageWrapper/PageWrapper.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './PageWrapper.scss';
 import SideNav from './SideNav/SideNav';
 
-export default function PageWrapper({ children }) {
+export default function PageWrapper({ children, fullscreen }) {
   return (
     <div className="PageWrapper">
       <SideNav />
-      <div className="inner">{children}</div>
+      <div className={`inner ${fullscreen ? 'fullscreen' : ''}`}>{children}</div>
     </div>
   );
 }
