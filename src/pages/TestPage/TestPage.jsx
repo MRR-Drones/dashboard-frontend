bcd4929c1baa85b022a07cd07c60386f7541f278
liekevdvoort <lieke.voort1@gmail.com>
2022-05-10 13:23:04 +0200

    update testpage

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -10,20 +10,22 @@ import Card from '../../components/Card/Card';
 export default function TestPage() {
   return (
     <PageWrapper>
-      <Button red fullwidth>
-        test
-      </Button>
-      <Button>test</Button>
-      <Input label="email" placeholder="test" />
-      <Input white placeholder="test" />
-      <div className="cards-grid">
-        <Card battery={20} speed={50} altitude={200} timeTravelled={23} number={1} status="offline" />
-        <Card battery={20} speed={50} altitude={200} timeTravelled={23} number={2} status="active" />
-      </div>
-      <div className="cards-grid-small">
-        <Card number={3} status="online" small />
-        <Card number={4} status="active" small />
-        <Card number={5} status="offline" small />
+      <div className="TestPage">
+        <Button red fullwidth>
+          test
+        </Button>
+        <Button>test</Button>
+        <Input label="email" placeholder="test" />
+        <Input white placeholder="test" />
+        <div className="cards-grid">
+          <Card battery={20} speed={50} altitude={200} timeTravelled={23} number={1} status="offline" />
+          <Card battery={20} speed={50} altitude={200} timeTravelled={23} number={2} status="active" />
+        </div>
+        <div className="cards-grid-small">
+          <Card number={3} status="online" small />
+          <Card number={4} status="active" small />
+          <Card number={5} status="offline" small />
+        </div>
       </div>
     </PageWrapper>
   );
