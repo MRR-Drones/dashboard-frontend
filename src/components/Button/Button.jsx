8d4bbfb73c02b50e25f925c1c349ee5c59e7ad58
liekevdvoort <lieke.voort1@gmail.com>
2022-06-01 10:20:46 +0200

    fix button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion';
 export default function Button({ children, onclick, fullwidth, red, outlined }) {
   return (
     <motion.button
-      whileHover={{ scale: 1.05 }}
+      whileHover={{ scale: outlined ? 1 : 1.05 }}
       transition={{ duration: 0.25 }}
       whileTap={{ scale: 0.75 }}
       onClick={onclick}
